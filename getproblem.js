3b68af7141b867f7070f8ec6c7a414f609f432c0
haruharo101 <haruharo101@gmail.com>
2023-01-09 15:48:49 +0900

    Ver.2301_01_016

--- getproblem.js
+++ getproblem.js
@@ -5,9 +5,10 @@ var problem_list = {
         for(let i=0; i<pr_list.length; i++) {
             let list_info = pr_list[i];
             let tmp = "<tr>";
-            for(let j=0; j<3; j++) {
+            for(let j=0; j<2; j++) {
                 tmp += "<td>" + String(list_info[j]) + "</td>";
             }
+            tmp += "<td><progress value=\""+String(list_info[2])+"\" max=\"20\"></progress> <span id=\"problem-dif\">"+String(list_info[2])+"</span></td>";
             tmp += "</tr>";
             $('#problem-list').append(tmp);
         }
@@ -19,10 +20,10 @@ var problem_list = {
 }
 
 function getproblem_list() {
-    pr_list.push(["100A", "정수 덧셈", "0"]);
-    pr_list.push(["100B", "정수 뺄셈", "0"]);
-    pr_list.push(["100C", "정수 곱셈", "0"]);
-    pr_list.push(["100D", "정수 나눗셈", "0"]);
+    pr_list.push(["100A", "정수 덧셈", 1]);
+    pr_list.push(["100B", "정수 뺄셈", 2]);
+    pr_list.push(["100C", "정수 곱셈", 3]);
+    pr_list.push(["100D", "정수 나눗셈", 4]);
 
     problem_list.print_problem_list();
 }
