fd41c772317d83b6b69b24298067958ec4f932e5
haruharo101 <haruharo101@gmail.com>
2023-01-10 02:18:28 +0900

    Ver.2301_04_003

--- src/get_contest.js
+++ src/get_contest.js
@@ -4,7 +4,7 @@ let contest_list = {
     print_contest_list:function () {
         for(let i=0; i<ct_list.length; i++) {
             let list_info = ct_list[i];
-            let tmp = "<tr>";
+            let tmp = "<tr class=\"contest_tr\">";
             for(let j=0; j<5; j++) {
                 tmp += "<td>" + String(list_info[j]) + "</td>";
             }
@@ -16,6 +16,7 @@ let contest_list = {
 
 function getcontest_list() {
     ct_list.push(["100", "Alpha Contest", "2023-13-32 09:00", "2023-13-32 16:00", 21474]);
+    ct_list.push(["101", "제-1회 미적확통컵", "2023-13-33 09:00", "2023-13-33 16:00", "0/0"]);
 
     contest_list.print_contest_list();
 }
