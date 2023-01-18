a0928587474739463b52e11f37c683f0056d91dc
haruharo101 <haruharo101@gmail.com>
2023-01-19 00:09:38 +0900

    Ver.2301_06_005

--- src/get_problem.js
+++ src/get_problem.js
@@ -4,12 +4,12 @@ var problem_list = {
     print_problem_list:function () {
         for(let i=0; i<pr_list.length; i++) {
             let list_info = pr_list[i];
-            let tmp = "<tr class=\"problem_tr\">";
+            let tmp = "<tr class=\"problem_tr\" onclick=\"location.href=\'problem\/" + String(list_info[0]) + ".html\'\">";
             for(let j=0; j<2; j++) {
                 tmp += "<td>" + String(list_info[j]) + "</td>";
             }
             tmp += "<td class=\"diff_td\"><progress class=\"diff_pg\" value=\""+String(list_info[2])+"\" max=\"20\"></progress> <span class=\"diff\" id=\"problem-dif\">"+String(list_info[2])+"</span></td>";
-            tmp += "</tr>";
+            tmp += "</a></tr>";
             $('#problem-list').append(tmp);
             
         }
@@ -21,22 +21,6 @@ var problem_list = {
 }
 
 function getproblem_list() {
-    pr_list.push(["100A", "정수 덧셈", 1]);
-    pr_list.push(["100B", "정수 뺄셈", 2]);
-    pr_list.push(["100C", "정수 곱셈", 3]);
-    pr_list.push(["100D", "정수 나눗셈", 4]);
-    pr_list.push(["100A", "정수 덧셈", 1]);
-    pr_list.push(["100B", "정수 뺄셈", 2]);
-    pr_list.push(["100C", "정수 곱셈", 3]);
-    pr_list.push(["100D", "정수 나눗셈", 4]);
-    pr_list.push(["100A", "정수 덧셈", 1]);
-    pr_list.push(["100B", "정수 뺄셈", 2]);
-    pr_list.push(["100C", "정수 곱셈", 3]);
-    pr_list.push(["100D", "정수 나눗셈", 4]);
-    pr_list.push(["100A", "정수 덧셈", 1]);
-    pr_list.push(["100B", "정수 뺄셈", 2]);
-    pr_list.push(["100C", "정수 곱셈", 3]);
-    pr_list.push(["100D", "정수 나눗셈", 4]);
     pr_list.push(["100A", "정수 덧셈", 1]);
     pr_list.push(["100B", "정수 뺄셈", 2]);
     pr_list.push(["100C", "정수 곱셈", 3]);
